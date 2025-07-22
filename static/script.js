425976cbb7c973d7a5fd214136bd56d556e1edb8
DEEPIKA K <deepika28226@gmail.com>
2025-07-22 21:28:32 +0530

    script.js

--- static/script.js
+++ static/script.js
@@ -1,1 +1,10 @@
 //this is a java script file
+const fileInput = document.getElementById('pic');
+const previewSection = document.getElementById('preview');
+const previewImage = document.getElementById('previewImage');
+const loadingSection = document.getElementById('load');
+const resultsSection = document.getElementById('results');
+const uploadArea = document.querySelector('.upl-area');
+
+let detectionActive = false;
+let uplimgpath = null;
